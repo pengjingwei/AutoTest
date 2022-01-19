fae7ed2d31ea18939373cbef0361e6d0e24fd1fd
hujuncai <jshujuncai@163.com>
2022-01-19 15:06:17 +0800

    对表添加脱敏规则

--- pages/settings_newDesData.py
+++ pages/settings_newDesData.py
@@ -9,67 +9,48 @@ from pages.login import Login
 
 
 class FiscalYEAR(Base):
-    def add_fiscal_year(self, loc, value):
+    dict_loc = {
+        '左侧目录加号展开预算信息': (By.XPATH, '//*[@id="demoTree"]/li/ul/li[1]/ul/li[3]/div/i[1]'),
+        '进入预算编制': (By.XPATH, '//*[@id="demoTree"]/li/ul/li[1]/ul/li[3]/ul/li[1]/div/cite'),
+        '编辑': (By.XPATH, '/html/body/div[1]/div/div[2]/div[3]/table/tbody/tr[1]/td[8]/div/div[1]/a[1]'),
+        '要脱敏的字段': (By.XPATH, '//*[@id="discernRuleId12"]/xm-select/div[2]'),
+        '敏感规则': (By.XPATH, '//*[@id="discernRuleId12"]/xm-select/div[3]/div/div/div[2]/div[13]/i'),
+        '脱敏条件': (By.XPATH, '/html/body/div[1]/form/div[1]/div/div[2]/table/tbody/tr[13]/td[9]/div/i'),
+        '选择角色': (By.XPATH, '//*[@id="role"]/xm-select/div[2]'),
+        '选中角色': (By.XPATH, '//*[@id="role"]/xm-select/div[3]/div/div/div[3]/div[4]/div'),
+        '点击确定': (By.XPATH, '//*[@id="layui-layer1"]/div[3]/a[1]'),
+        '点击保存': (By.XPATH, '//*[@id="layui-layer2"]/div[3]/a[1]')
+    }
+
+    def add_fiscal_year(self):
         self.switch_window()
         self.switch_frame('indexSrc')
-        # 预算信息+号定位
-        budget_information = (By.XPATH, '//*[@id="demoTree"]/li/ul/li[1]/ul/li[3]/div/i[1]')
-
         # 点击展开预算信息
-        self.click(budget_information)
-        # 预算编制定位
-        budget_making = (By.XPATH, '//*[@id="demoTree"]/li/ul/li[1]/ul/li[3]/ul/li[1]/div/cite')
+        self.click(self.dict_loc['左侧目录加号展开预算信息'])
         # 点击预算编制
-        self.click(budget_making)
-
-        # 编辑定位
-        edit = (By.XPATH, '/html/body/div[1]/div/div[2]/div[3]/table/tbody/tr[1]/td[8]/div/div[1]/a[1]')
-        # 点击编辑
-        self.click(edit)
-
-        # 切换到新的窗口
-        # handles = Base.switch_window(index=-1)
-
+        self.click(self.dict_loc['进入预算编制'])
+        # 点击“单位收入预算表”的编辑按钮
+        self.click(self.dict_loc['编辑'])
         self.switch_frame('layui-layer-iframe2')
         # 定位滚动条导致不可见的元素
-        # 第十三个字段--FISCAL_YEAR字段
-        select_item = (By.XPATH, '//*[@id="discernRuleId12"]/xm-select/div[2]')
-        # 点击该元素
-        self.scroll_click(select_item)
-
-        #self.quit_frame()
-        # self.switch_frame('layui-layer-iframe4')
+        # 第十三个字段
+        self.scroll_click(self.dict_loc['要脱敏的字段'])
         # 定位敏感规则--MASK
-        fascalyear_test = (By.XPATH, '//*[@id="discernRuleId12"]/xm-select/div[3]/div/div/div[2]/div[13]/i')
-        self.scroll_click(fascalyear_test)
-        # self.switch_frame('indexSrc')
-
+        self.scroll_click(self.dict_loc['敏感规则'])
         # 点击脱敏条件
-        choose_role_edit = (By.XPATH, '/html/body/div[1]/form/div[1]/div/div[2]/table/tbody/tr[13]/td[9]/div/i')
-        self.click(choose_role_edit)
-
+        self.click(self.dict_loc['脱敏条件'])
         # 进入iframe
         self.switch_frame('layui-layer-iframe1')
-
         # 点击选择角色框
-        choose_role = (By.XPATH, '//*[@id="role"]/xm-select/div[2]')
-        self.click(choose_role)
+        self.click(self.dict_loc['选择角色'])
         # 选择“然”角色
-        select_role = (By.XPATH, '//*[@id="role"]/xm-select/div[3]/div/div/div[3]/div[4]/div')
-        self.scroll_click(select_role)
-
+        self.scroll_click(self.dict_loc['选中角色'])
         self.parent_frame()
-
         # 点击确定
-        click_save = (By.XPATH, '//*[@id="layui-layer1"]/div[3]/a[1]')
-        self.click(click_save)
+        self.click(self.dict_loc['点击确定'])
         self.parent_frame()
-
-
-        #self.parent_frame()
-        # 定位保存
-        save = (By.XPATH, '//*[@id="layui-layer2"]/div[3]/a[1]')
-        self.click(save)
+        # 点击保存
+        self.click(self.dict_loc['点击保存'])
 
 
 if __name__ == '__main__':
@@ -79,9 +60,7 @@ if __name__ == '__main__':
     login.login('admin', '123456')
     enter = HomeEnter(driver)
     enter.enter_front('id', '308', 'xpath', '//*[@title="脱敏设置"]')
-
     addfisalyear = FiscalYEAR(driver)
-    addfisalyear.add_fiscal_year(By.XPATH, '//*[@id="discernRuleId12"]/xm-select/div[3]/div/div/div[2]/div[19]/div')
-
+    addfisalyear.add_fiscal_year()
     sleep(3)
     driver.quit()
