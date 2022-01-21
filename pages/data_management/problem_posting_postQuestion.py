4dfe6ffd104c54a62604cb66aa65f1a77631e366
pjw <1571933050@qq.com>
2022-01-21 09:27:15 +0800

    【数据管控】-【问题发布】-【发布问题】

--- pages/data_management/problem_posting_postQuestion.py
+++ pages/data_management/problem_posting_postQuestion.py
@@ -1,8 +1,11 @@
 from time import sleep
 
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from base.base import Base
+from pages.home_page import HomeEnter
+from pages.login import Login
 
 
 class PostQuestion(Base):
@@ -12,7 +15,7 @@ class PostQuestion(Base):
         '选择用户': (By.XPATH, '//*[@id="userId"]/../div/div'),
         'jkl1109': (By.XPATH, '//dd[text()="jkl1109"]'),
         '保存': (By.XPATH, '//*[text()="保存"]'),
-        '确定': (By.XPATH, '//*[text()="确定"]'),
+        '确定': (By.XPATH, '//a[text()="确定"]'),
         '断言返回值': (By.XPATH, '//*[text()="发布成功！"]')
     }
 
@@ -30,6 +33,7 @@ class PostQuestion(Base):
         self.click(self.dict_loc['保存'])
 
         result = self.get_text(self.dict_loc['断言返回值'])
+        sleep(1)
         self.click(self.dict_loc['确定'])
 
         sleep(1)
@@ -37,3 +41,17 @@ class PostQuestion(Base):
         self.switch_window(0)
 
         return result
+
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    login = Login(driver)
+    login.login('admin', '123456')
+
+    enter = HomeEnter(driver)
+    enter.enter_front('id', '303', 'xpath', '//*[text()="问题发布"]')
+
+    post = PostQuestion(driver)
+    res = post.post()
+    print(res)
+    driver.quit()
